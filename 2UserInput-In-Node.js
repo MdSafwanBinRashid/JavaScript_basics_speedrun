32c63113eb47783e220885e202bf6e5f0bd62276
Md Safwan Bin Rashid <mdsafwanbinrashid2@gmail.com>
2025-11-30 22:33:10 -0600

    Update 2UserInput-In-Node.js

--- 2UserInput-In-Node.js
+++ 2UserInput-In-Node.js
@@ -1,6 +1,6 @@
 // Node Package Manager (NPM) is used in JS projects in order to handle different dependencies
 // npm init -y (creates package.json)
-// npm install prompt-sync (creates package-lock,json and node_modules)
+// npm install prompt-sync (creates package-lock.json and node_modules)
 
 const prompt = require("prompt-sync")() // import prompt-sync to get user input
 const v = prompt("type something: ") // store the input in variable 'v'
